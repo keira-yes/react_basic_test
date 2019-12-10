c9fe55b91cae1d6d869f57b7e3ecdafdf4982679
Irina <irina.kirillova@wdg.com.ua>
2019-12-10 19:05:39 +0200

    add fetch to get gallery data

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,6 +2,29 @@ import React from "react";
 import GalleryItem from './GalleryItem';
 
 export default class Gallery extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      gallery: []
+    }
+  }
+
+  getGalleryItems = () => {
+    const link = 'https://www.reddit.com/r/reactjs.json?limit=100';
+    fetch(link)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data.data);
+      })
+  };
+
+  componentDidMount() {
+    this.getGalleryItems();
+  }
+
   render() {
     return(
       <div className='gallery'>
